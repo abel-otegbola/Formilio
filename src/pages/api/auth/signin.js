2bcaa6242d25e0b4b40a29e36df31c947eef7b2b
abel-otegbola <abel15655@gmail.com>
2023-02-10 01:47:29 +0100

    fix: remove post condition in signin

--- src/pages/api/auth/signin.js
+++ src/pages/api/auth/signin.js
@@ -5,31 +5,24 @@ import { compare } from "bcryptjs";
 export default async function handler(req, res) {
     await connectMongo().catch(error => res.json({ error: "Connection Failed"}))
 
-    if(req.method === "POST") {
-        if(!req.body) return res.status(404).json({ error: "Don't have form data" })
-        const { email, password } = req.body;
+    if(!req.body) return res.status(404).json({ error: "Don't have form data" })
+    const { email, password } = req.body;
 
-        //Check user
-        const checkexisting = await Users.findOne({ email });
-        if(checkexisting) {
-            //Check hased password with DB password
-            const checkPassword = await compare(password, checkexisting.password);
+    //Check user
+    const checkexisting = await Users.findOne({ email });
+    if(checkexisting) {
+        //Check hased password with DB password
+        const checkPassword = await compare(password, checkexisting.password);
 
-            //Incorrect password - send response
-            if (!checkPassword) {
-                res.status(400).json({error: 'Password doesnt match'});
-            }
-
-            //Else send success response
-            return res.status(200).json({ msg: "Login successful", data: checkexisting });
-        }
-        else {
-            res.status(400).json({ error: "User does not exist" })
+        //Incorrect password - send response
+        if (!checkPassword) {
+            res.status(400).json({error: 'Password doesnt match'});
         }
 
-
+        //Else send success response
+        return res.status(200).json({ msg: "Login successful", data: checkexisting });
     }
     else {
-        res.status(500).json({ error: "HTTPS Method not valid" })
+        res.status(400).json({ error: "User does not exist" })
     }
 }
