5fef695444fcac0264083ac337c88f3e9b029ece
abel-otegbola <abel15655@gmail.com>
2023-03-02 16:38:44 +0100

    feat: add notifications page

---
+++ src/app/(views)/dashboard/notifications/page.js
@@ -0,0 +1,62 @@
+'use client'
+import Header from "@/components/header";
+import { convert } from "@/helper/convertDate";
+import { getInitials } from "@/helper/getInitials";
+import { useState } from "react";
+import { FaCheckCircle, FaCommentAlt } from "react-icons/fa";
+
+export default function Notifications() {
+    const [notifications, setNotifications] = useState([
+        {id: 1, createdAt: "02-03-2023T12:00", msg: "Welcome to formilio. We are glad to have you here", sender: "Formilio", opened: false}
+    ])
+    const [active, setActive] = useState(1)
+
+    return (
+        <div className="px-4">
+            <Header text={"Notifications"} icon={<FaCommentAlt />}>
+                <p className="text-white">View all your notifications here</p>
+            </Header>
+            {
+                notifications.length === 0 ?
+                <div className="h-[70px] flex justify-center items-center">
+                    <p>You have no notifications</p>
+                </div>
+                :
+                <div className="flex">
+                    <div className="md:w-[30%]">
+                    {
+                        notifications.map(notification => (
+                            <div key={notification.id} onClick={() => setActive(notification.id)} className="flex md:flex-no-wrap flex-wrap items-center justify-between bg-gray-100 dark:bg-gray-900 border border-transparent border-y-gray-300/[0.2] hover:bg-blue hover:text-white cursor-pointer">
+                                <p className="px-[10px] border-2 border-white/[0.3] bg-fuchsia-500/[0.1] md:block hidden uppercase text-[12px] font-semibold m-3 shadow-lg rounded-full">{getInitials(notification.sender || "user")}</p>
+                                <div className="flex-1 items-center overflow-x-auto">
+                                    {notification.sender}
+                                </div>
+                                <div className="flex items-center p-1">
+                                    <p className="pl-2 text-[10px]">{convert(notification.createdAt)}</p>
+                                    <FaCheckCircle className="text-green-400 p-3 text-4xl cursor-pointer"/>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    </div>
+                    <div className="md:w-[70%]">
+                    {
+                        notifications.filter(element => element.id === active).map(item => (
+                            <div key={item.id} className="md:px-[5%] px-2">
+                                <div className="">
+                                    <h3 className="text-xl font-semibold">{item.sender}</h3>
+                                    <div className="flex items-center">
+                                        <p className="text-[10px]">{convert(item.createdAt)}</p>
+                                        <FaCheckCircle className="text-green-400 p-3 text-4xl cursor-pointer"/>
+                                    </div>
+                                    <div className="my-2 py-4 px-4 bg-gray-100 dark:bg-gray-800">{item.msg}</div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
