c4a6870810eabfaf5ca140daad2eca2e1b54c15d
abel-otegbola <abel15655@gmail.com>
2023-03-04 17:00:48 +0100

    feat: add email-templates for sending emails

--- src/pages/api/endpoint/[...slug].js
+++ src/pages/api/endpoint/[...slug].js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Email = require('email-templates')
 
 import connectMongo from "@/database/connection";
 import { Endpoints, Submissions } from "@/model/Schema";
@@ -20,11 +21,41 @@ export default async function handler(req, res) {
   async function submitAction(user) {
     try{
       await Submissions.create({ key: slug[0], user, data: JSON.stringify(req.body)})
+      sendEmail(req.body.fullname, req.body.email, user, req.body.message)
       res.redirect(303, `${process.env.NEXTAUTH_URL}/thankyou`)
     }
     catch(err){
-      res.redirect(303, `${process.env.NEXTAUTH_URL}/error`);
+      console.log(err)
+      res.json({error: err});
     }
   }
 
+}
+
+
+function sendEmail(name, user, useremail, message) {
+  const email = new Email({
+    message: {
+        from: "abel.d.otegbola@gmail.com"
+    },
+    send: true,
+    transport: {
+        jsonTransport: true
+    },
+    subjectPrefix: process.env.NEXTAUTH_URL === 'http://localhost:3000' ? false : `[${process.env.NEXTAUTH_URL.toUpperCase()}]`
+  });
+  email.send({
+      template: "templates",
+      message: {
+          to: user
+      },
+      locals: {
+        name,
+        email: useremail,
+        user,
+        message
+      }
+  })
+  .then(console.log("Email sent"))
+  .catch(console.error)
 }
