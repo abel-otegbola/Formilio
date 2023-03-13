8aeb9b128754be9e5302c3509d2de44aa476fff3
abel-otegbola <abel15655@gmail.com>
2023-03-13 04:51:49 +0100

    feat: add colors to endpoints in dashboard

--- src/app/(views)/dashboard/page.js
+++ src/app/(views)/dashboard/page.js
@@ -6,7 +6,7 @@ import Header from "@/components/header";
 import SubmissionChart from "@/components/charts/submissionChart";
 import SubmissionList from "@/components/submissionsList";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
 import GenerateEndpoint from "@/components/generateEndpoint";
 
@@ -15,6 +15,39 @@ export default function Dashboard() {
     const [submissions, setSubmissions] = useState([])
     const [endpoints, setEndpoints] = useState([])
     const { data: session } = useSession()   
+    const [endpointsTitles, setEndpointsTitles] = useState(['Loading', 'Loading', 'Loading'])
+    const [endpointsData, setEndpointsData] = useState([0,0,0])
+    const colors = [
+        'rgba(227, 97, 200, 0.7)',
+        'rgba(92, 82, 242, 0.7)',
+        'rgba(239, 159, 11, 0.7)',
+        'rgba(255, 165, 0, 0.7)',
+        'rgba(0, 128, 128, 0.7)',
+        'rgba(0, 191, 255, 0.7)',
+        'rgba(199, 21, 133, 0.7)'
+    ]
+
+
+    useEffect(() => {
+
+        //set Endpoints
+        let endpointsTitles = endpoints?.map(item => ( item.title )) || []
+
+        setEndpointsTitles(endpointsTitles)
+
+        // get the key fields from endpoints and submissions, store into an array
+        let endpointsKeys = endpoints?.map(item => ( item.key )) || []
+        let submissionsKeys = submissions?.map(item => ( item.key )) || []
+
+        //Find number of each submissions for all endpoints using the keys.
+        let resultArray = []
+
+        for(let i=0; i<endpointsKeys.length; i++) {
+            resultArray.push(submissionsKeys.filter(item => item === endpointsKeys[i]).length)
+        }
+
+        setEndpointsData(resultArray)
+    }, [endpoints, submissions])
 
     return (
         <div className="px-4">
@@ -38,16 +71,20 @@ export default function Dashboard() {
                     <h4 className="p-2 font-semibold text-blue">LATEST ENDPOINTS</h4>
                     {/* <p className="p-2">Statistics of submissions for your latest endpoints</p> */}
                     <div className="relative flex justify-center items-center md:mx-[15%] mx-[5%] my-4 bg-white dark:bg-gray-900 rounded-full md:w-auto w-fit">
-                        <EndpointsChart endpoints={endpoints} submissions={submissions}/>
+                        <EndpointsChart endpointsTitles={endpointsTitles} endpointsData={endpointsData}/>
                         <div className="absolute flex justify-center items-center h-[60%] w-[60%]">
                             <h3 className="text-blue font-semibold text-3xl ">70%</h3>
                         </div>
                     </div>
                     <EndpointsList 
                         setEndpoints={setEndpoints} 
-                        limit={5}  
+                        limit={5} 
+                        colors={colors} 
+                        data={endpointsData}
                     />
-                    
+                    <div className="mt-10 flex">
+                        <a href="/dashboard/endpoints" className="p-3 px-6 text-center rounded-full border border-blue text-blue hover:bg-blue hover:text-white">View all endpoints</a>
+                    </div>
                 </div>
             </div>
         </div>
