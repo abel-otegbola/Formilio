c3f298c8aef7ed6b190c5699435d1a6017f60a1c
abel-otegbola <abel15655@gmail.com>
2023-07-26 21:32:52 +0100

    refactor: remove react-emails dependency for faster build

--- src/components/templates/emailTemplate.js
+++ src/components/templates/emailTemplate.js
@@ -1,30 +1,26 @@
-import { Html } from '@react-email/html';
-import { Text } from '@react-email/text';
-import { Button } from '@react-email/button';
-
 export function EmailTemplate(props) {
   const { url, formdata, data, respond } = props;
 
   return (
-    <Html lang="en">
+    <html lang="en">
       {
         respond ?
         <>
-          <Text style={{ padding: 5 }}>{data.autoRespond}</Text>
+          <p style={{ padding: 5 }}>{data.autoRespond}</p>
         </>
         : 
         <>
-          <Text style={{ padding: 5, fontWeight: "700", fontSize: "20px"  }}>New submission 👋</Text>
-          <Text style={{ padding: 5, fontWeight: "700"  }}>You have a new submission from {formdata.email || formdata.fullname || formdata.name} in your {data.title} endpoint</Text>
+          <p style={{ padding: 5, fontWeight: "700", fontSize: "20px"  }}>New submission 👋</p>
+          <p style={{ padding: 5, fontWeight: "700"  }}>You have a new submission from {formdata.email || formdata.fullname || formdata.name} in your {data.title} endpoint</p>
           {
             Object.keys(formdata).splice(0, 3).map((key, index) => (
               <div key={index} style={{ lineHeight: "15px", marginBlock: "5px", padding: 10, backgroundColor: "rgba(100, 100, 100, 0.2)" }}>
-                <Text style={{ paddingInline: 5, textTransform: "uppercase" }} >{key}:</Text>
-                <Text style={{ paddingInline: 5,  }}>{formdata[key]}</Text>
+                <p style={{ paddingInline: 5, pTransform: "uppercase" }} >{key}:</p>
+                <p style={{ paddingInline: 5,  }}>{formdata[key]}</p>
               </div>
           ))
           }
-          <Button style={{ padding: 10, borderRadius: 5, margin: 5, marginTop: 10, backgroundColor: "#6252f2", color: "#fff" }} href={url}>Check it out here</Button>
+          <button style={{ padding: 10, borderRadius: 5, margin: 5, marginTop: 10, backgroundColor: "#6252f2", color: "#fff" }} href={url}>Check it out here</button>
 
           <div style={{ display: "flex", justifyContent: "center", marginBlock: 20 }}>Get all your form submissions using our endpoints generator. <a href="https://formilio.com" style={{ color: "#6252f2" }}>Check it out</a></div>
           
@@ -32,6 +28,6 @@ export function EmailTemplate(props) {
           <div style={{ display: "flex", justifyContent: "center" }}>Formilio @Copyright {new Date().getFullYear()} || Designed and created by <a href="https://abelo.tech" style={{ color: "#6252f2" }}>Abelo</a></div>
         </>
       }
-    </Html>
+    </html>
   );
 }
