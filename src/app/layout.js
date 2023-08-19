4046b6ad529eda406a5610a0efcdeee38f576586
abel-otegbola <abel15655@gmail.com>
2023-08-19 17:29:20 +0100

    feat: add theme effect globally

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,17 @@ import Topbar from '@/components/general/topbar'
 import './globals.css'
 import AppFooter from '@/components/general/footer'
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from 'react';
 
 export default function RootLayout({ children, session }) {
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  })
 
   return (
     <html lang='en'>
